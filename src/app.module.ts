cfe72a4b81b93e41dd0839c25ec9f924d0f71da6
Kauan Portela <kauan.portela@wisereducacao.com>
2023-05-22 19:23:37 -0300

    feat: added typeorm to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
