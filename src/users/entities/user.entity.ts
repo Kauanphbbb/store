b8cd582137d5ea6a51781208c9c2cf5e8f2eebd6
Kauan Portela <kauan.portela@wisereducacao.com>
2023-05-22 19:24:21 -0300

    feat: user entity created

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  age: number;
+  @Column()
+  lastName: string;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
