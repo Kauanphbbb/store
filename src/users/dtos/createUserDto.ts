1a7851858f8b83d0f1ab07d7a3cab963a116fff8
Kauan Portela <kauan.portela@wisereducacao.com>
2023-05-22 17:59:30 -0300

    feat: create dto to create a user

---
+++ src/users/dtos/createUserDto.ts
@@ -0,0 +1,14 @@
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly email: string;
+  @IsString()
+  readonly password: string;
+  @IsString()
+  readonly name: string;
+  @IsNumber()
+  readonly age: number;
+  @IsString()
+  readonly lastName: string;
+}
