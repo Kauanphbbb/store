486dccd6d974c56b70e485431824c52e09508908
Kauan Portela <kauan.portela@wisereducacao.com>
2023-05-23 17:54:32 -0300

    fix: deleted products module for now

--- src/products/products.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductsController } from './products.controller';
-import { ProductsRepository } from './products.repository';
-
-@Module({
-  providers: [ProductsRepository],
-  controllers: [ProductsController],
-})
-export class ProductsModule {}
